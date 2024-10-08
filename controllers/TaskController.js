637c20edb415e38c8ec2e542c98dbfeb24e6518d
Hurryep Technology <hurryepworks@gmail.com>
2024-10-08 11:37:29 +0530

    Update TaskController.js

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -93,7 +93,7 @@ exports.updateTasks = CatchAsyncError(async(req,res,next)=>{
 //     })
 // })
 
-exports.memberTaskUpdate = CatchAsyncError(async(req,res)=>{
+exports.memberTaskUpdate = CatchAsyncError(async(req,res,next)=>{
 
 
     const taskId = req.params.id
